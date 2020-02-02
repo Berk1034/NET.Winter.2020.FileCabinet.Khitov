d563f8d93e51c0c34fbdaa5c66fa790fb9f180cb
Constantin Hitov <kostya.khitov@mail.ru>
2020-02-02 13:20:51 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetRecord
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
