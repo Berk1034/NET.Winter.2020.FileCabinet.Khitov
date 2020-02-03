07ab072128235de60ee5b793f0cc97cbb9a4e64c
Constantin Hitov <kostya.khitov@mail.ru>
2020-02-03 16:29:58 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,53 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short grade, decimal height, char favouriteSymbol)
         {
+            if (firstName is null)
+            {
+                throw new ArgumentNullException(nameof(firstName), "Firstname can't be null.");
+            }
+
+            if (firstName.Trim().Length == 0)
+            {
+                throw new ArgumentException("Firstname cannot contain only spaces.", nameof(firstName));
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException("Firstname length should be in range [2;60].", nameof(firstName));
+            }
+
+            if (lastName is null)
+            {
+                throw new ArgumentNullException(nameof(lastName), "Lastname can't be null.");
+            }
+
+            if (lastName.Trim().Length == 0)
+            {
+                throw new ArgumentException("Firstname cannot contain only spaces.", nameof(lastName));
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException("Firstname length should be in range [2;60].", nameof(lastName));
+            }
+
+            DateTime minimalDate = new DateTime(1950, 1, 1);
+
+            if (dateOfBirth < minimalDate || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("Date should start from 01-Jan-1950 till Now.");
+            }
+
+            if (grade < -10 || grade > 10)
+            {
+                throw new ArgumentException("Grade should be in range [-10;10].");
+            }
+
+            if (height < 0.3m || height > 3m)
+            {
+                throw new ArgumentException("Height can't be lower 40cm and higher than 3m.");
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
