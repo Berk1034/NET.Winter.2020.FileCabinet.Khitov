c3db39567f00ea417c9bb816541870b530a5e51f
Constantin Hitov <kostya.khitov@mail.ru>
2020-03-03 19:05:38 +0300

    Create FileCabinetGenerator. Add command line parameters support. Implement generator for FileCabinetRecord fields. Add export of generated records to CSV file. Add export of generated records to XML file.

---
+++ FileCabinetGenerator/Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FileCabinetGenerator
+{
+    public class Name
+    {
+        [XmlAttribute("first")]
+        public string FirstName { get; set; }
+
+        [XmlAttribute("last")]
+        public string LastName { get; set; }
+    }
+}
