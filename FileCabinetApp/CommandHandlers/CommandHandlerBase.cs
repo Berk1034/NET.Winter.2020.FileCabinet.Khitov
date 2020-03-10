306e6f65eeadea6f0335a9e2668a605e4bad1747
Constantin Hitov <kostya.khitov@mail.ru>
2020-03-10 10:12:14 +0300

    Introduce chain of responsibility for command handlers.

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>
+    /// The CommandHandlerBase class.
+    /// </summary>
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        /// <summary>
+        /// Executes the request.
+        /// </summary>
+        /// <param name="appCommandRequest">The app command request.</param>
+        public abstract void Handle(AppCommandRequest appCommandRequest);
+
+        /// <summary>
+        /// Sets the next command handler.
+        /// </summary>
+        /// <param name="commandHandler">The command handler.</param>
+        public void SetNext(ICommandHandler commandHandler)
+        {
+            this.nextHandler = commandHandler;
+        }
+    }
+}
