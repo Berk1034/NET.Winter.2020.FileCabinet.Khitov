e853e9e819c4826aa5e6c4e7aca5eb3aa27a09e5
Constantin Hitov <kostya.khitov@mail.ru>
2020-03-21 15:28:11 +0300

    Add similar commands hint.

--- FileCabinetApp/CommandHandlers/MissedCommandHandler.cs
+++ FileCabinetApp/CommandHandlers/MissedCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FileCabinetApp.CommandHandlers
@@ -9,14 +10,59 @@ namespace FileCabinetApp.CommandHandlers
     /// </summary>
     public class MissedCommandHandler : CommandHandlerBase
     {
+        private static string[] availableCommands = new string[]
+        {
+            "create",
+            "insert",
+            "find",
+            "update",
+            "delete",
+            "list",
+            "stat",
+            "purge",
+            "import",
+            "export",
+            "help",
+            "exit",
+        };
+
         /// <summary>
         /// Executes the request.
         /// </summary>
         /// <param name="appCommandRequest">The app command request.</param>
         public override void Handle(AppCommandRequest appCommandRequest)
         {
-            Console.WriteLine($"There is no '{appCommandRequest.Command}' command.");
+            Console.WriteLine($"There is no '{appCommandRequest.Command}' command. See 'help'. ");
             Console.WriteLine();
+
+            IEnumerable<string> mostSimilarCommands = new List<string>();
+
+            string command = appCommandRequest.Command;
+            while (!mostSimilarCommands.Any() && command.Length > 0)
+            {
+                string commandSubstr = command;
+                mostSimilarCommands = availableCommands.Where(command => command.StartsWith(commandSubstr, StringComparison.InvariantCulture));
+                command = command.Substring(0, command.Length - 1);
+            }
+
+            int commandsCount = mostSimilarCommands.Count();
+
+            if (mostSimilarCommands.Any())
+            {
+                if (commandsCount == 1)
+                {
+                    Console.WriteLine("The most similar command is");
+                }
+                else
+                {
+                    Console.WriteLine("The most similar commands are");
+                }
+
+                foreach (var similarCommand in mostSimilarCommands)
+                {
+                    Console.WriteLine(similarCommand.PadLeft(similarCommand.Length + 8));
+                }
+            }
         }
     }
 }
